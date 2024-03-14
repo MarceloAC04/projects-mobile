0e5544060f5a50c76dd4f2ad9f8a84681774bd3a
Marcelo Araújo Cardoso <48260449846@info.local>
2024-03-14 10:52:33 -0300

    finalizado o cameraExemplo

--- cameraExemplo/App.js
+++ cameraExemplo/App.js
@@ -1,25 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Alert, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import { Camera, CameraType } from 'expo-camera';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+
+import { FontAwesome } from '@expo/vector-icons';
+import { Image } from 'react-native';
+
+import * as MediaLibrary from 'expo-media-library'
 
 export default function App() {
-  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back)
+  const cameraRef= useRef(null)
 
-useEffect(() => {
-  (async () => {
-    const {status: cameraStatus} = await Camera.requestCameraPermissionsAsync()
-  })();
-},[])
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back)
+  const [photo, setPhoto] = useState(null)
+  const [openModal, setOpenModal] = useState(false)
+
+  async function CapturePhoto() {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      setPhoto(photo.uri)
+
+      setOpenModal(true)
+      console.log( photo)
+    }
+  }
+
+  function ClearPhoto() {
+    setPhoto(null)
+
+    setOpenModal(false)
+  }
+
+  async function SavePhoto(){
+    if (photo) {
+      await MediaLibrary.createAssetAsync(photo)
+      .then(() => {
+        Alert.alert('Sucesso', 'foto salva na galeria')
+      }).catch(erro => {
+        alert("Error ao processar foto")
+      })
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync()
+
+      const { status: mediaStatus } = await MediaLibrary.requestPermissionsAsync()
+    })();
+  }, [])
 
 
   return (
     <View style={styles.container}>
-      <Camera 
-      type={cameraType}
-      style={styles.camera}
-      
+      <StatusBar/>
+      <Camera
+      ref={cameraRef}
+        type={cameraType}
+        style={styles.camera}
+
       >
         <View style={styles.viewFlip}>
           <TouchableOpacity style={styles.bntFLip} onPress={() => setCameraType(cameraType == CameraType.front ? CameraType.back : CameraType.front)}>
@@ -27,6 +67,32 @@ useEffect(() => {
           </TouchableOpacity>
         </View>
       </Camera>
+
+      <TouchableOpacity style={styles.bntCapture} onPress={() => CapturePhoto()}>
+        <FontAwesome name='camera' size={23} color={'#fff'}/>
+      </TouchableOpacity>
+
+      <Modal animationType='slide' transparent={false} visible={openModal}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', padding: 30}}>
+        <Image style={{width: '100%', height: 500, borderRadius: 10}} source={{uri: photo}}/>
+
+        <View style={{margin: 10, flexDirection: 'row'}}>
+          <TouchableOpacity 
+            style={styles.btnCancel}
+            onPress={() => ClearPhoto()}
+          >
+            <FontAwesome name='trash' size={35} color={'#ff0000'}/>
+          </TouchableOpacity>
+
+          <TouchableOpacity 
+            style={styles.btnUpload}
+            onPress={() => SavePhoto()}
+          >
+            <FontAwesome name='save' size={35} color={'#121212'}/>
+          </TouchableOpacity>
+        </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -57,5 +123,32 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#fff',
     marginBottom: 20
+  },
+  bntCapture: {
+    margin: 20,
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: '#121212',
+
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  btnCancel: {
+    margin: 20,
+    padding: 15,
+    borderRadius: 15,
+    backgroundColor: 'transparent',
+
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  btnUpload: {
+    margin: 20,
+    padding: 15,
+    borderRadius: 15,
+    backgroundColor: 'transparent',
+
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
